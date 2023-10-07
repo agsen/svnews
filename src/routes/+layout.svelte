c6e743299ffbc905a6cc5c6d4e8d75bf9062bd4c
Agung Setyo Nugroho <agung.nugroho@stockbit.com>
2023-10-07 23:01:39 +0700

    feat: setting +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
+	import relativeTime from 'dayjs/plugin/relativeTime';
+	import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+	import dayjs from 'dayjs';
+
+	dayjs.extend(relativeTime);
+	dayjs.extend(localizedFormat);
+
+	const queryClient = new QueryClient({
+		defaultOptions: { queries: { refetchOnWindowFocus: false } }
+	});
+</script>
+
+<QueryClientProvider client={queryClient}>
+	<Navbar />
+	<div class="px-6 dark:bg-slate-900 min-h-screen"><slot /></div>
+</QueryClientProvider>
