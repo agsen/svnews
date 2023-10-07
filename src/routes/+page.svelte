b6a16653f21931886b35955f1b085c78cdd45a65
Agung Setyo Nugroho <agung.nugroho@stockbit.com>
2023-10-07 23:02:12 +0700

    feat: update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { useQuery } from '@sveltestack/svelte-query';
+	import { Button, Spinner } from 'flowbite-svelte';
+	import type { Article, NewsResponse } from '../interfaces';
+	import apiRequest from '../utils/apiRequest';
+	import CardNews from '../components/CardNews.svelte';
+	import cleanDataArticles from '../utils/cleanDataArticles';
+
+	let page = 1;
+	let allArticles: Article[] = [];
+
+	$: result = useQuery(['headline', page], () =>
+		apiRequest({
+			method: 'GET',
+			url: 'top-headlines',
+			params: {
+				country: 'id',
+				page,
+				pageSize: 17
+			}
+		}).then((res) => res?.data as NewsResponse)
+	);
+
+	$: {
+		const list = cleanDataArticles($result?.data?.articles);
+		allArticles = [...allArticles, ...list];
+	}
+
+	const handleLoadMore = () => {
+		page = page + 1;
+	};
+</script>
+
+<div class="pt-8 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+	{#each allArticles as news}
+		<CardNews {news} />
+	{/each}
+</div>
+
+{#if $result.isLoading}
+	<div class="flex justify-center items-center w-full h-screen">
+		<Spinner />
+	</div>
+{/if}
+
+<Button class="w-full my-6" on:click={handleLoadMore}>Load more ...</Button>
